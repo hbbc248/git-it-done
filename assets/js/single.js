5be9a505671663a6a29b46ca306f06d81fbb86ab
Ibrahim <ibrahimzerlin@hotmail.com>
2021-03-17 10:42:29 -0500

    completed link between pages and error handling

--- assets/js/single.js
+++ assets/js/single.js
@@ -1,8 +1,10 @@
 var issueContainerEl = document.querySelector("#issues-container");
 var limitWarningEl = document.querySelector("#limit-warning");
+var repoNameEl = document.querySelector("#repo-name");
 
 var getRepoIssues = function(repo) {
     var apiUrl = "https://api.github.com/repos/" + repo + "/issues?direction=asc";
+    // make request to URL
     fetch(apiUrl).then(function(response) {
         // request was succesful
         if (response.ok) {
@@ -16,7 +18,8 @@ var getRepoIssues = function(repo) {
                 }
             });
         } else {
-            alert("There was a problem with your request!");
+            // if not succesful, redirect to homepage
+            document.location.replace("./index.html");
         }
     });
 };
@@ -75,5 +78,21 @@ var displayWarning = function(repo) {
     // append to warning container
     limitWarningEl.appendChild(linkEl);
 };
-  
-getRepoIssues("facebook/react");
+
+var getRepoName = function() {
+    // grab repo name from url query string
+    var queryString = document.location.search;  
+    var repoName = queryString.split("=")[1];
+
+    //check if repoName exist
+    if (repoName) {
+        // display repo name in page
+        repoNameEl.textContent = repoName;
+        getRepoIssues(repoName);
+    } else {
+        // if no repo was given, redirect to the homepage
+        document.location.replace("./index.html")
+    }
+};
+
+getRepoName();
